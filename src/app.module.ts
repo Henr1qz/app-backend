ab0ccfa8742f44e7779fb53b03523be4a31905d0
Henr1qz <henriquezotti246@gmail.com>
2026-08-21 12:39:08 -0300

    app-backen, aula programação web e mobile

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { ProfilesModule } from './profiles/profiles.module';
+
+
+@Module({
+  imports: [UsersModule, ProfilesModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
